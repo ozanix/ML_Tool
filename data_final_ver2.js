656d4aa567e4e740b9266e3e29b47f54d3691ddb
Erol Ozan <gerolozan@hotmail.com>
2021-04-07 12:19:35 -0400

    Add files via upload

---
+++ data_final_ver2.js
@@ -0,0 +1,359 @@
+/**
+ * @license
+ * Copyright 2018 Google LLC. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =============================================================================
+ */
+ //selected_size = parseInt(document.getElementById("selected_size").value);
+ number_of_images = parseInt(document.getElementById("myText").value);
+
+ number_of_classes = parseInt(document.getElementById("number_of_classes").value);
+
+ selected_size = parseInt(document.getElementById("selected_size").value);
+
+ file_name = document.getElementById("file_name").value;
+
+ file_extension = document.getElementById("file_extension").value;
+
+ number_of_training_images = parseInt(document.getElementById("number_of_training_images").value);
+
+ number_of_test_images = parseInt(document.getElementById("number_of_test_images").value);
+
+ epochs_value = parseInt(document.getElementById("epochs_value").value);
+
+var IMAGE_SIZE = selected_size*selected_size;
+
+
+
+
+var NUM_TEST_ELEMENTS = number_of_test_images;
+//const NUM_TEST_ELEMENTS = 270;
+
+//const MNIST_IMAGES_SPRITE_PATH =
+//    'training_imgs.png';
+//    'https://storage.googleapis.com/learnjs-data/model-builder/mnist_labels_uint8';
+
+/**
+ * A class that fetches the sprited MNIST dataset and returns shuffled batches.
+ *
+ * NOTE: This will get much easier. For now, we do data fetching and
+ * manipulation manually.
+ */
+
+
+export class MnistData {
+  constructor() {
+    this.shuffledTrainIndex = 0;
+    this.shuffledTestIndex = 0;
+  }
+
+  async load() {
+    // Make a request for the MNIST sprited image.
+  //  const img = new Image();
+    var final_image_array = [0,1,0];//
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+    const imgRequest = new Promise((resolve, reject) => {
+    //  img.crossOrigin = '';
+    //  img.onload = () => {
+    //    img.width = img.naturalWidth;
+    //    img.height = img.naturalHeight;
+
+        const datasetBytesBuffer =
+            new ArrayBuffer(number_of_images * IMAGE_SIZE * 4);
+
+        const chunkSize = number_of_images;
+        canvas.width = selected_size;
+        canvas.height = chunkSize*selected_size;
+
+        for (let i = 0; i < 1; i++) {
+          const datasetBytesView = new Float32Array(
+              datasetBytesBuffer, i * IMAGE_SIZE * chunkSize * 4,
+              IMAGE_SIZE * chunkSize);
+      //    ctx.drawImage(img,0,0);
+
+        //  const imageData = ctx.getImageData(0, 0, 56, 56*400);
+var imageDataX = image_array;
+console.log("imageDataX");
+console.log(imageDataX);
+          for (let j = 0; j < imageDataX.length / 4; j++) {
+            // All channels hold an equal value since the image is grayscale, so
+            // just read the red channel.
+            //datasetBytesView[j] = imageData.data[j * 4] / 255;
+              datasetBytesView[j] = ((parseInt(imageDataX[j * 4]) + parseInt(imageDataX[j * 4 +1])+ parseInt(imageDataX[j * 4 +2]) )/3)/ 255;
+//console.log(j, ",", datasetBytesView[j]);
+          }
+          final_image_array=datasetBytesView;
+
+        }
+        console.log("image data set loading started");
+        this.datasetImages = new Float32Array(final_image_array);
+
+console.log("loading is succesful");
+        resolve();
+    });
+
+    const [imgResponse, labelsResponse] =
+        await Promise.all([imgRequest]);
+
+
+                var dataLabels = [0,1];
+                var k = 0;
+                var ratio = parseInt(number_of_images/number_of_classes);
+
+                var i=0;
+                for (i = 0; i < (ratio); i++) {
+
+                      k=number_of_classes * i;
+                      dataLabels[k] = 1;
+                      dataLabels[k +1] = 0;
+                      if (number_of_classes>2){dataLabels[k +2] = 0;}
+                      if (number_of_classes>3){dataLabels[k +3] = 0;}
+                      if (number_of_classes>4){dataLabels[k +4] = 0;}
+                      if (number_of_classes>5){dataLabels[k +5] = 0;}
+                      if (number_of_classes>6){dataLabels[k +6] = 0;}
+                      if (number_of_classes>7){dataLabels[k +7] = 0;}
+                      if (number_of_classes>8){dataLabels[k +8] = 0;}
+                      if (number_of_classes>9){dataLabels[k +9] = 0;}
+
+
+                }
+        for (i = ratio; i < (2*(ratio)); i++) {
+                  k=number_of_classes* i;
+                  dataLabels[k] = 0;
+                  dataLabels[k +1] = 1;
+                  if (number_of_classes>2){dataLabels[k +2] = 0;}
+                  if (number_of_classes>3){dataLabels[k +3] = 0;}
+                  if (number_of_classes>4){dataLabels[k +4] = 0;}
+                  if (number_of_classes>5){dataLabels[k +5] = 0;}
+                  if (number_of_classes>6){dataLabels[k +6] = 0;}
+                  if (number_of_classes>7){dataLabels[k +7] = 0;}
+                  if (number_of_classes>8){dataLabels[k +8] = 0;}
+                  if (number_of_classes>9){dataLabels[k +9] = 0;}
+
+
+                }
+if (number_of_classes>2){
+                for (i = (2*(ratio)); i < (3*(ratio)); i++) {
+                          k=number_of_classes* i;
+                          dataLabels[k] = 0;
+                          dataLabels[k +1] = 0;
+                          if (number_of_classes>2){dataLabels[k +2] = 1;}
+                          if (number_of_classes>3){dataLabels[k +3] = 0;}
+                          if (number_of_classes>4){dataLabels[k +4] = 0;}
+                          if (number_of_classes>5){dataLabels[k +5] = 0;}
+                          if (number_of_classes>6){dataLabels[k +6] = 0;}
+                          if (number_of_classes>7){dataLabels[k +7] = 0;}
+                          if (number_of_classes>8){dataLabels[k +8] = 0;}
+                          if (number_of_classes>9){dataLabels[k +9] = 0;}
+
+
+                        }
+                      }
+if (number_of_classes>3){
+                        for (i = (3*(ratio)); i < (4*(ratio)); i++) {
+                                  k=number_of_classes* i;
+                                  dataLabels[k] = 0;
+                                  dataLabels[k +1] = 0;
+                                  if (number_of_classes>2){dataLabels[k +2] = 0;}
+                                  if (number_of_classes>3){dataLabels[k +3] = 1;}
+                                  if (number_of_classes>4){dataLabels[k +4] = 0;}
+                                  if (number_of_classes>5){dataLabels[k +5] = 0;}
+                                  if (number_of_classes>6){dataLabels[k +6] = 0;}
+                                  if (number_of_classes>7){dataLabels[k +7] = 0;}
+                                  if (number_of_classes>8){dataLabels[k +8] = 0;}
+                                  if (number_of_classes>9){dataLabels[k +9] = 0;}
+
+                                }
+                              }
+
+if (number_of_classes>4){
+
+                                for (i = (4*(ratio)); i < (5*(ratio)); i++) {
+                                          k=number_of_classes* i;
+                                          dataLabels[k] = 0;
+                                          dataLabels[k +1] = 0;
+                                          if (number_of_classes>2){dataLabels[k +2] = 0;}
+                                          if (number_of_classes>3){dataLabels[k +3] = 0;}
+                                          if (number_of_classes>4){dataLabels[k +4] = 1;}
+                                          if (number_of_classes>5){dataLabels[k +5] = 0;}
+                                          if (number_of_classes>6){dataLabels[k +6] = 0;}
+                                          if (number_of_classes>7){dataLabels[k +7] = 0;}
+                                          if (number_of_classes>8){dataLabels[k +8] = 0;}
+                                          if (number_of_classes>9){dataLabels[k +9] = 0;}
+
+                                        }
+                                      }
+
+if (number_of_classes>5){
+                                       for (i = (5*(ratio)); i < (6*(ratio)); i++) {
+                                                  k=number_of_classes* i;
+                                                  dataLabels[k] = 0;
+                                                  dataLabels[k +1] = 0;
+                                                  if (number_of_classes>2){dataLabels[k +2] = 0;}
+                                                  if (number_of_classes>3){dataLabels[k +3] = 0;}
+                                                  if (number_of_classes>4){dataLabels[k +4] = 0;}
+                                                  if (number_of_classes>5){dataLabels[k +5] = 1;}
+                                                  if (number_of_classes>6){dataLabels[k +6] = 0;}
+                                                  if (number_of_classes>7){dataLabels[k +7] = 0;}
+                                                  if (number_of_classes>8){dataLabels[k +8] = 0;}
+                                                  if (number_of_classes>9){dataLabels[k +9] = 0;}
+
+
+                                                }
+
+                                              }
+if (number_of_classes>6){
+
+                                                for (i = (6*(ratio)); i < (7*(ratio)); i++) {
+                                                          k=number_of_classes* i;
+                                                          dataLabels[k] = 0;
+                                                          dataLabels[k +1] = 0;
+                                                          if (number_of_classes>2){dataLabels[k +2] = 0;}
+                                                          if (number_of_classes>3){dataLabels[k +3] = 0;}
+                                                          if (number_of_classes>4){dataLabels[k +4] = 0;}
+                                                          if (number_of_classes>5){dataLabels[k +5] = 0;}
+                                                          if (number_of_classes>6){dataLabels[k +6] = 1;}
+                                                          if (number_of_classes>7){dataLabels[k +7] = 0;}
+                                                          if (number_of_classes>8){dataLabels[k +8] = 0;}
+                                                          if (number_of_classes>9){dataLabels[k +9] = 0;}
+
+                                                        }
+                                                      }
+
+if (number_of_classes>7){
+
+                                                        for (i = (7*(ratio)); i < (8*(ratio)); i++) {
+                                                                  k=number_of_classes* i;
+                                                                  dataLabels[k] = 0;
+                                                                  dataLabels[k +1] = 0;
+                                                                  if (number_of_classes>2){dataLabels[k +2] = 0;}
+                                                                  if (number_of_classes>3){dataLabels[k +3] = 0;}
+                                                                  if (number_of_classes>4){dataLabels[k +4] = 0;}
+                                                                  if (number_of_classes>5){dataLabels[k +5] = 0;}
+                                                                  if (number_of_classes>6){dataLabels[k +6] = 0;}
+                                                                  if (number_of_classes>7){dataLabels[k +7] = 1;}
+                                                                  if (number_of_classes>8){dataLabels[k +8] = 0;}
+                                                                  if (number_of_classes>9){dataLabels[k +9] = 0;}
+}
+                                                                }
+
+
+if (number_of_classes>8){
+                                                                for (i = (8*(ratio)); i < (9*(ratio)); i++) {
+                                                                          k=number_of_classes* i;
+                                                                          dataLabels[k] = 0;
+                                                                          dataLabels[k +1] = 0;
+                                                                          if (number_of_classes>2){dataLabels[k +2] = 0;}
+                                                                          if (number_of_classes>3){dataLabels[k +3] = 0;}
+                                                                          if (number_of_classes>4){dataLabels[k +4] = 0;}
+                                                                          if (number_of_classes>5){dataLabels[k +5] = 0;}
+                                                                          if (number_of_classes>6){dataLabels[k +6] = 0;}
+                                                                          if (number_of_classes>7){dataLabels[k +7] = 0;}
+                                                                          if (number_of_classes>8){dataLabels[k +8] = 1;}
+                                                                          if (number_of_classes>9){dataLabels[k +9] = 0;}
+
+                                                                        }
+                                                                        }
+
+if (number_of_classes>9){
+
+                                                                        for (i = (9*(ratio)); i < (10*(ratio)); i++) {
+                                                                                  k=number_of_classes* i;
+                                                                                  dataLabels[k] = 0;
+                                                                                  dataLabels[k +1] = 0;
+                                                                                  if (number_of_classes>2){dataLabels[k +2] = 0;}
+                                                                                  if (number_of_classes>3){dataLabels[k +3] = 0;}
+                                                                                  if (number_of_classes>4){dataLabels[k +4] = 0;}
+                                                                                  if (number_of_classes>5){dataLabels[k +5] = 0;}
+                                                                                  if (number_of_classes>6){dataLabels[k +6] = 0;}
+                                                                                  if (number_of_classes>7){dataLabels[k +7] = 0;}
+                                                                                  if (number_of_classes>8){dataLabels[k +8] = 0;}
+                                                                                  if (number_of_classes>9){dataLabels[k +9] = 1;}
+
+                                                                                }
+                                                                              }
+
+
+console.log("**********************");
+              console.log("dataLabels");
+        console.log(dataLabels);
+        console.log("**********************");
+                this.datasetLabels  = new Uint8Array(dataLabels);
+
+
+
+
+    console.log("this.datasetLabels", this.datasetLabels);
+
+    // Create shuffled indices into the train/test set for when we select a
+    // random dataset element for training / validation.
+    this.trainIndices = tf.util.createShuffledIndices(number_of_training_images);
+    console.log("this.trainIndices");
+    console.log(this.trainIndices);
+    this.testIndices = tf.util.createShuffledIndices(number_of_test_images);//attention
+
+    // Slice the the images and labels into train and test sets.
+    this.trainImages =
+        this.datasetImages.slice(0, IMAGE_SIZE * number_of_training_images);
+    this.testImages = this.datasetImages.slice(IMAGE_SIZE * number_of_test_images);
+    this.trainLabels =
+        this.datasetLabels.slice(0, number_of_classes * number_of_training_images);
+        console.log("this.trainLabels");
+        console.log(this.trainLabels);
+    this.testLabels =
+        this.datasetLabels.slice(number_of_classes * number_of_test_images);
+        console.log("this.testLabels");
+        console.log(this.testLabels);
+
+  }
+
+  nextTrainBatch(batchSize) {
+    return this.nextBatch(
+        batchSize, [this.trainImages, this.trainLabels], () => {
+          this.shuffledTrainIndex =
+              (this.shuffledTrainIndex + 1) % this.trainIndices.length;
+          return this.trainIndices[this.shuffledTrainIndex];
+        });
+  }
+
+  nextTestBatch(batchSize) {
+    return this.nextBatch(batchSize, [this.testImages, this.testLabels], () => {
+      this.shuffledTestIndex =
+          (this.shuffledTestIndex + 1) % this.testIndices.length;
+      return this.testIndices[this.shuffledTestIndex];
+    });
+  }
+
+  nextBatch(batchSize, data, index) {
+    const batchImagesArray = new Float32Array(batchSize * IMAGE_SIZE);
+    const batchLabelsArray = new Uint8Array(batchSize * number_of_classes);
+
+    for (let i = 0; i < batchSize; i++) {
+      const idx = index();
+
+      const image =
+          data[0].slice(idx * IMAGE_SIZE, idx * IMAGE_SIZE + IMAGE_SIZE);
+      batchImagesArray.set(image, i * IMAGE_SIZE);
+
+      const label =
+          data[1].slice(idx * number_of_classes, idx * number_of_classes + number_of_classes);
+      batchLabelsArray.set(label, i * number_of_classes);
+    }
+
+    const xs = tf.tensor2d(batchImagesArray, [batchSize, IMAGE_SIZE]);
+    const labels = tf.tensor2d(batchLabelsArray, [batchSize, number_of_classes]);
+
+    return {xs, labels};
+  }
+}
---
+++ script_main_ver2.js
@@ -0,0 +1,276 @@
+
+model_file_name=document.getElementById("model_file_name").value;
+number_of_images = parseInt(document.getElementById("myText").value);
+
+number_of_classes = parseInt(document.getElementById("number_of_classes").value);
+
+selected_size = parseInt(document.getElementById("selected_size").value);
+
+file_name = document.getElementById("file_name").value;
+
+file_extension = document.getElementById("file_extension").value;
+
+number_of_training_images = parseInt(document.getElementById("number_of_training_images").value);
+
+batch_size_entered = parseInt(document.getElementById("batch_size_entered").value);
+
+
+
+epochs_value = parseInt(document.getElementById("epochs_value").value);
+
+
+import {MnistData} from './data_final_ver2.js';
+
+
+async function showExamples(data) {
+  number_of_images = parseInt(document.getElementById("myText").value);
+
+  number_of_classes = parseInt(document.getElementById("number_of_classes").value);
+
+  selected_size = parseInt(document.getElementById("selected_size").value);
+
+  file_name = document.getElementById("file_name").value;
+
+  file_extension = document.getElementById("file_extension").value;
+
+  number_of_training_images = parseInt(document.getElementById("number_of_training_images").value);
+
+  epochs_value = parseInt(document.getElementById("epochs_value").value);
+  // Create a container in the visor
+  const surface =
+    tfvis.visor().surface({ name: 'Input Data Examples', tab: 'Input Data'});
+
+  const examples = data.nextTrainBatch(20);
+
+  const numExamples = examples.xs.shape[0];
+
+
+
+  var str="";
+   str = document.getElementById("class_names").value;
+    var classNames = str.split(",");
+selected_size = parseInt(document.getElementById("selected_size").value);
+
+  for (let i = 0; i < numExamples; i++) {
+    const imageTensor = tf.tidy(() => {
+      // Reshape the image to 28x28 px
+      return examples.xs
+        .slice([i, 0], [1, examples.xs.shape[1]])
+        .reshape([selected_size, selected_size, 1]);
+    });
+
+    const canvas = document.createElement('canvas');
+    canvas.width = selected_size;
+    canvas.height = selected_size;
+    canvas.style = 'margin: 4px;';
+    await tf.browser.toPixels(imageTensor, canvas);
+    surface.drawArea.appendChild(canvas);
+
+    imageTensor.dispose();
+  }
+}
+
+async function run() {
+  console.log("Content loaded, ready");
+  document.getElementById("training_started").addEventListener("click", start_training);
+
+}
+async function start_training() {
+
+
+  number_of_images = parseInt(document.getElementById("myText").value);
+
+  number_of_classes = parseInt(document.getElementById("number_of_classes").value);
+
+  selected_size = parseInt(document.getElementById("selected_size").value);
+
+  file_name = document.getElementById("file_name").value;
+
+  file_extension = document.getElementById("file_extension").value;
+
+  number_of_training_images = parseInt(document.getElementById("number_of_training_images").value);
+
+  epochs_value = parseInt(document.getElementById("epochs_value").value);
+
+model_file_name = document.getElementById("model_file_name").value;
+  starting_number = parseInt(document.getElementById("starting_number").value);
+  const data = new MnistData();
+  await data.load();
+  await showExamples(data);
+  const model = getModel();
+tfvis.show.modelSummary({name: 'Model Architecture', tab: 'Model'}, model);
+
+await train(model, data);
+await showAccuracy(model, data);
+await showConfusion(model, data);
+
+await model.save('downloads://'+ model_file_name);
+
+
+}
+
+document.addEventListener('DOMContentLoaded',run);
+
+
+
+function getModel() {
+  const model = tf.sequential();
+selected_size = parseInt(document.getElementById("selected_size").value);
+  const IMAGE_WIDTH = selected_size;
+  const IMAGE_HEIGHT = selected_size;
+  const IMAGE_CHANNELS = 1;
+
+  // In the first layer of our convolutional neural network we have
+  // to specify the input shape. Then we specify some parameters for
+  // the convolution operation that takes place in this layer.
+  model.add(tf.layers.conv2d({
+    inputShape: [selected_size, selected_size, IMAGE_CHANNELS],
+    kernelSize: 5,
+    filters: 8,
+    strides: 1,
+    activation: 'relu',
+    kernelInitializer: 'varianceScaling'
+  }));
+
+  // The MaxPooling layer acts as a sort of downsampling using max values
+  // in a region instead of averaging.
+  model.add(tf.layers.maxPooling2d({poolSize: [2, 2], strides: [2, 2]}));
+
+  // Repeat another conv2d + maxPooling stack.
+  // Note that we have more filters in the convolution.
+  model.add(tf.layers.conv2d({
+    kernelSize: 10,
+    filters: 16,
+    strides: 1,
+    activation: 'relu',
+    kernelInitializer: 'varianceScaling'
+  }));
+
+
+
+  model.add(tf.layers.maxPooling2d({poolSize: [2, 2], strides: [2, 2]}));
+
+  // Now we flatten the output from the 2D filters into a 1D vector to prepare
+  // it for input into our last layer. This is common practice when feeding
+  // higher dimensional data to a final classification output layer.
+  model.add(tf.layers.flatten());
+
+  // Our last layer is a dense layer which has 10 output units, one for each
+  // output class (i.e. 0, 1, 2, 3, 4, 5, 6, 7, 8, 9).
+  //change here
+  const NUM_OUTPUT_CLASSES = parseInt(document.getElementById("number_of_classes").value);
+  number_of_classes = NUM_OUTPUT_CLASSES;
+  model.add(tf.layers.dense({
+    units: NUM_OUTPUT_CLASSES,
+    kernelInitializer: 'varianceScaling',
+    activation: 'softmax'
+  }));
+
+
+  // Choose an optimizer, loss function and accuracy metric,
+  // then compile and return the model
+  const optimizer = tf.train.adam();
+  model.compile({
+    optimizer: optimizer,
+    loss: 'categoricalCrossentropy',
+    metrics: ['accuracy'],
+  });
+
+  return model;
+}
+
+
+async function train(model, data) {
+  const metrics = ['loss', 'val_loss', 'acc', 'val_acc'];
+  const container = {
+    name: 'Model Training', tab: 'Model', styles: { height: '1000px' }
+  };
+  const fitCallbacks = tfvis.show.fitCallbacks(container, metrics);
+
+
+  const BATCH_SIZE = batch_size_entered;
+  const TRAIN_DATA_SIZE = number_of_training_images;
+const TEST_DATA_SIZE = number_of_test_images;
+  const [trainXs, trainYs] = tf.tidy(() => {
+    const d = data.nextTrainBatch(TRAIN_DATA_SIZE);
+
+    return [
+      d.xs.reshape([TRAIN_DATA_SIZE, selected_size, selected_size, 1]),
+      d.labels
+
+    ];
+
+
+  });
+
+
+
+  const [testXs, testYs] = tf.tidy(() => {
+    const d = data.nextTestBatch(TEST_DATA_SIZE);
+
+    return [
+      d.xs.reshape([TEST_DATA_SIZE, selected_size, selected_size, 1]),
+      d.labels
+
+    ];
+  });
+
+
+  return model.fit(trainXs, trainYs, {
+    batchSize: BATCH_SIZE,
+    validationData: [testXs, testYs],
+    epochs: epochs_value,
+    shuffle: true,
+    callbacks: fitCallbacks
+  });
+
+
+}
+
+
+str = document.getElementById("class_names").value;
+ classNames = str.split(",");
+function doPrediction(model, data, testDataSize = number_of_test_images) {
+  const IMAGE_WIDTH = parseInt(document.getElementById("selected_size").value);
+  const IMAGE_HEIGHT = parseInt(document.getElementById("selected_size").value);
+  const testData = data.nextTestBatch(testDataSize);
+  const testxs = testData.xs.reshape([testDataSize, selected_size, selected_size, 1]);
+  const labels = testData.labels.argMax(-1);
+    console.log("testxs");
+  console.log(testxs);
+  const preds = model.predict(testxs).argMax(-1);
+  console.log("testData.labels.");
+  console.log(testData.labels);
+  testxs.dispose();
+  console.log("testData");
+console.log(testData);
+  return [preds, labels];
+}
+
+
+async function showAccuracy(model, data) {
+  const [preds, labels] = doPrediction(model, data);
+  console.log("preds");
+  console.log(preds);
+  console.log("labels");
+  console.log(labels);
+  const classAccuracy = await tfvis.metrics.perClassAccuracy(labels, preds);
+  const container = {name: 'Accuracy', tab: 'Evaluation'};
+  var stro = document.getElementById("class_names").value;
+  var classNames2 = stro.split(",");
+  tfvis.show.perClassAccuracy(container, classAccuracy, classNames2);
+  const confusionMatrix = await tfvis.metrics.confusionMatrix(labels, preds);
+  const container2 = {name: 'Confusion Matrix', tab: 'Evaluation'};
+  tfvis.render.confusionMatrix(container2, {values: confusionMatrix, tickLabels: classNames2});
+
+  labels.dispose();
+}
+
+async function showConfusion(model, data) {
+  //const [preds, labels] = doPrediction(model, data);
+  //const confusionMatrix = await tfvis.metrics.confusionMatrix(labels, preds);
+  //const container = {name: 'Confusion Matrix', tab: 'Evaluation'};
+  //tfvis.render.confusionMatrix(container, {values: confusionMatrix, tickLabels: classNames});
+
+//  labels.dispose();
+}
